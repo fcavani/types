95009e2e56583c8b12ee1a7b27f3922472b89787
Felipe Cavani <fcavani@gmail.com>
2015-10-16 16:06:34 -0300

    initial commit

---
+++ types.go
@@ -0,0 +1,387 @@
+// Copyright 2015 Felipe A. Cavani. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// Start date:		2010-08-11
+
+// Package types have functions to create an instatiation of one type from the type name.
+package types
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"runtime"
+	"strings"
+	"time"
+)
+
+var typemap map[string]reflect.Type
+
+func init() {
+	typemap = make(map[string]reflect.Type, 100)
+	Insert(errors.New(""))
+	InsertName("os.errorString", errors.New(""))
+	Insert("")
+	Insert(new(string))
+	Insert(int(0))
+	Insert(uint(0))
+	Insert(new(int))
+	Insert(false)
+	Insert(new(bool))
+	Insert(float64(3.14))
+	Insert(new(float64))
+	Insert(time.Time{})
+	Insert(&time.Time{})
+	Insert(time.Duration(0))
+}
+
+// Dump the name and the type from the type base.
+func Dump() {
+	for key, t := range typemap {
+		fmt.Println(key, t)
+	}
+}
+
+func pkgname() (name string) {
+	pc, _, _, ok := runtime.Caller(4)
+	if !ok {
+		return
+	}
+	f := runtime.FuncForPC(pc)
+	s := strings.SplitN(f.Name(), ".", 2)
+	if len(s) != 2 {
+		return
+	}
+	name = s[0]
+	return
+}
+
+func findpkgname(t reflect.Type) (name string) {
+	switch t.Kind() {
+	case reflect.Array, reflect.Chan, reflect.Map, reflect.Ptr, reflect.Slice:
+		name = findpkgname(t.Elem())
+		if name == "" {
+			name = t.PkgPath()
+		}
+	default:
+		name = t.PkgPath()
+	}
+	return
+}
+
+func replacepkgname(in string, t reflect.Type) (out string) {
+	pkg := findpkgname(t)
+	s := strings.Split(pkg, "/")
+	if len(s) <= 0 {
+		return
+	}
+	out = strings.Replace(in, s[len(s)-1], pkg, 1)
+	return
+}
+
+func nameof(t reflect.Type) (name string) {
+	defer func() {
+		if name == "projects2/web/context.Data" {
+			panic("error")
+		}
+	}()
+	n := t.Name()
+	if t.Kind() == reflect.Interface || n == "" {
+		name = replacepkgname(t.String(), t)
+		if name == "" {
+			name = t.String()
+		}
+	} else {
+		pkg := t.PkgPath()
+		if pkg == "" {
+			name = n
+		} else {
+			name = pkg + "." + n
+		}
+	}
+	return
+}
+
+// NameOf returns the package name and the name of the type
+func NameOf(t reflect.Type) string {
+	return nameof(t)
+}
+
+// Name accepts a variable of any type and returns the package
+// name and the name of the type or a function
+func Name(i interface{}) string {
+	val := reflect.ValueOf(i)
+	if !val.IsValid() {
+		return ""
+	}
+	t := val.Type()
+	switch t.Kind() {
+	case reflect.Func:
+		return runtime.FuncForPC(val.Pointer()).Name()
+	default:
+		return nameof(t)
+	}
+}
+
+// InsertType insertes a type for future instantiation.
+// Do this in the same package where the type was declared.
+// The use of  init function is advised.
+func InsertType(t reflect.Type) {
+	tname := nameof(t)
+	if _, found := typemap[tname]; !found {
+		typemap[tname] = t
+	}
+}
+
+// Insert type for future instantiation.
+// Do this in the same package where the type was declared.
+// The use of  init function is advised.
+func Insert(i interface{}) {
+	t := reflect.ValueOf(i).Type()
+	tname := nameof(t)
+	if _, found := typemap[tname]; !found {
+		typemap[tname] = t
+	}
+}
+
+// InsertName inserts a new type with the name.
+func InsertName(tname string, i interface{}) {
+	t := reflect.ValueOf(i).Type()
+	if _, found := typemap[tname]; !found {
+		typemap[tname] = t
+	}
+}
+
+// Type returns the Type from the type name.
+func Type(tname string) reflect.Type {
+	if t, found := typemap[tname]; found {
+		return t
+	}
+	panic("Type not found: " + tname)
+}
+
+// GetType return the type represented by tname
+func GetType(tname string) (reflect.Type, error) {
+	t, found := typemap[tname]
+	if !found {
+		return nil, errors.New("type not found: " + tname)
+	}
+	return t, nil
+
+}
+
+// IsEqualName compares the value type name with one name.
+func IsEqualName(val reflect.Value, tname string) bool {
+	return nameof(val.Type()) == tname
+}
+
+// MakeZero creates a zero value type for the type name.
+func MakeZero(tname string) reflect.Value {
+	return reflect.Zero(Type(tname))
+}
+
+// MakeNew create a new value from the type's name
+func MakeNew(tname string, bufcap int) (val reflect.Value) {
+	t := Type(tname)
+	val = MakeNewType(t, bufcap)
+	return
+}
+
+// MakeNewType creates a new value with type t.
+func MakeNewType(t reflect.Type, bufcap int) (val reflect.Value) {
+	switch t.Kind() {
+	case reflect.Ptr:
+		val = reflect.New(t).Elem()
+		val.Set(reflect.New(val.Type().Elem()))
+	case reflect.Chan:
+		//typ := reflect.ChanOf(reflect.BothDir, t.Elem())
+		val = reflect.New(t).Elem()
+		val.Set(reflect.MakeChan(t, bufcap)) //TODO: set buf?
+	case reflect.Slice:
+		val = reflect.New(t).Elem()
+		val.Set(reflect.MakeSlice(t, 0, bufcap))
+		val.SetLen(bufcap)
+	case reflect.Map:
+		val = reflect.New(t).Elem()
+		val.Set(reflect.MakeMap(t))
+	default:
+		val = reflect.New(t).Elem()
+	}
+	return
+}
+
+func isRecursive(v reflect.Value) bool {
+	ind := reflect.Indirect(v)
+	t := v.Type()
+	if ind.Kind() != reflect.Struct {
+		return false
+	}
+	for i := 0; i < ind.Type().NumField(); i++ {
+		ft := ind.Field(i).Type()
+		return ft == t || ft == reflect.PtrTo(t) || t.AssignableTo(ft) || reflect.PtrTo(t).AssignableTo(ft)
+	}
+	return false
+}
+
+//AllocStructPtrs find pointers in a struct and alloc than recursivily.
+func AllocStructPtrs(v reflect.Value) {
+	val := reflect.Indirect(v)
+	t := val.Type()
+
+	if t.Kind() == reflect.Struct {
+		for i := 0; i < t.NumField(); i++ {
+			field := val.Field(i)
+			switch field.Type().Kind() {
+			case reflect.Ptr:
+				v := MakeNewType(field.Type(), 0)
+				if isRecursive(v) {
+					//panic(fmt.Sprintf("struct %v have a field of the same type of this struct", NameOf(v.Type())))
+					continue
+				}
+				AllocStructPtrs(v)
+				if field.CanSet() {
+					field.Set(v)
+				}
+			case reflect.Slice:
+				v := MakeNewType(field.Type(), 0)
+				if field.CanSet() {
+					field.Set(v)
+				}
+			default:
+				continue
+			}
+		}
+	}
+	return
+}
+
+// Make instantiate a value of t type and allocate pointer and slices.
+func Make(t reflect.Type) (val reflect.Value) {
+	val = MakeNewType(t, 0)
+	AllocStructPtrs(val)
+	return
+}
+
+type deepcopy map[reflect.Value]reflect.Value
+
+func (d deepcopy) copy(src reflect.Value) (dst reflect.Value) {
+	switch src.Kind() {
+	case reflect.Bool:
+		dst = reflect.New(src.Type()).Elem()
+		dst.SetBool(src.Bool())
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		dst = reflect.New(src.Type()).Elem()
+		dst.SetInt(src.Int())
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		dst = reflect.New(src.Type()).Elem()
+		dst.SetUint(src.Uint())
+	case reflect.Uintptr:
+		panic("Uintptr isn't supported")
+	case reflect.Float32, reflect.Float64:
+		dst = reflect.New(src.Type()).Elem()
+		dst.SetFloat(src.Float())
+	case reflect.Complex64, reflect.Complex128:
+		dst = reflect.New(src.Type()).Elem()
+		dst.SetComplex(src.Complex())
+	case reflect.Array:
+		dst = reflect.New(src.Type()).Elem()
+		for i := 0; i < src.Type().Len(); i++ {
+			dst.Index(i).Set(d.copy(src.Index(i)))
+		}
+	case reflect.Chan:
+		dst = reflect.New(src.Type()).Elem()
+		dst.Set(reflect.MakeChan(src.Type(), src.Cap()))
+	case reflect.Func:
+		dst = src
+	case reflect.Interface:
+		dst = reflect.New(src.Type()).Elem()
+		if !src.Elem().IsValid() {
+			return
+		}
+		dst.Set(d.copy(src.Elem()))
+	case reflect.Map:
+		dst = reflect.New(src.Type()).Elem()
+		dst.Set(reflect.MakeMap(src.Type()))
+		for _, key := range src.MapKeys() {
+			dst.SetMapIndex(d.copy(key), d.copy(src.MapIndex(key)))
+		}
+	case reflect.Ptr:
+		if dst, found := d[src]; found {
+			return dst
+		}
+		dst = reflect.New(src.Type()).Elem()
+		d[src] = dst
+		if !src.Elem().IsValid() {
+			return
+		}
+		dst.Set(reflect.New(src.Type().Elem()))
+		val := d.copy(src.Elem())
+		dst.Elem().Set(val)
+	case reflect.Slice:
+		dst = reflect.New(src.Type()).Elem()
+		dst.Set(reflect.MakeSlice(src.Type(), src.Len(), src.Cap()))
+		for i := 0; i < src.Len(); i++ {
+			dst.Index(i).Set(d.copy(src.Index(i)))
+		}
+	case reflect.String:
+		dst = reflect.New(src.Type()).Elem()
+		dst.SetString(src.String())
+	case reflect.Struct:
+		dst = reflect.New(src.Type()).Elem()
+		for i := 0; i < src.Type().NumField(); i++ {
+			if dst.Field(i).CanSet() {
+				dst.Field(i).Set(d.copy(src.Field(i)))
+			}
+		}
+	case reflect.UnsafePointer:
+		panic("UnsafePointer isn't supported")
+	case reflect.Invalid:
+		return
+	default:
+		panic(fmt.Sprintf("kind %v is not supported", src.Kind()))
+	}
+	return
+}
+
+// Copy create a new value with all data of src copied into it.
+func Copy(src reflect.Value) reflect.Value {
+	d := make(deepcopy)
+	return d.copy(src)
+}
+
+//AnySettableValue find if exist one value that you can set.
+func AnySettableValue(val reflect.Value) bool {
+	switch val.Kind() {
+	case reflect.Array:
+		for i := 0; i < val.Type().Len(); i++ {
+			if AnySettableValue(val.Index(i)) {
+				return true
+			}
+		}
+	case reflect.Interface:
+		return AnySettableValue(val.Elem())
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			if AnySettableValue(val.MapIndex(key)) {
+				return true
+			}
+		}
+	case reflect.Ptr:
+		return AnySettableValue(val.Elem())
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			if AnySettableValue(val.Index(i)) {
+				return true
+			}
+		}
+	case reflect.Struct:
+		for i := 0; i < val.Type().NumField(); i++ {
+			if AnySettableValue(val.Field(i)) {
+				return true
+			}
+		}
+	default:
+		return val.CanSet()
+	}
+	return false
+}
---
+++ types_test.go
@@ -0,0 +1,336 @@
+// Copyright 2015 Felipe A. Cavani. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// Start date:		2010-09-21
+// Last modification:	2010-
+
+package types
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+type testitem struct {
+	t    interface{}
+	name string
+	val  interface{}
+}
+
+type testvec0 []int
+type testvec1 []testitem
+type teststr string
+
+var boolean bool = true
+var ptrbool *bool = &boolean
+var interger int = 42
+var ptrint *int = &interger
+var interger32 int32 = 42
+var ptrint32 *int32 = &interger32
+var interger8 uint8 = 42
+var ptrint8 *uint8 = &interger8
+var cadeia string = "outrastring"
+var ptrcadeia *string = &cadeia
+
+type channel chan bool
+
+var ptrChannel *channel
+
+var tests []testitem = []testitem{
+	{true, "bool", true},
+	{new(bool), "*bool", ptrbool},
+	{int(0), "int", 42},
+	{new(int), "*int", ptrint},
+	{int32(0), "int32", int32(64)},
+	{new(int32), "*int32", ptrint32},
+	{byte(0), "uint8", uint8(1)},
+	{new(byte), "*uint8", ptrint8},
+	{"string", "string", "istoeumastring"},
+	{new(string), "*string", ptrcadeia},
+	{struct{}{}, "struct {}", struct{}{}},
+	{&struct{}{}, "*struct {}", &struct{}{}},
+	{testitem{}, "github.com/fcavani/util/types.testitem", testitem{int(0), "int", 0}},
+	{&testitem{}, "*github.com/fcavani/util/types.testitem", &testitem{int(0), "int", 1}},
+	{[3]int{}, "[3]int", [3]int{1, 2, 3}},
+	{[]int{}, "[]int", []int{1, 2, 3}},
+	{[2]testitem{}, "[2]github.com/fcavani/util/types.testitem", [2]testitem{{int(9), "int", 0}, {int(9), "int", 2}}},
+	{[]testitem{}, "[]github.com/fcavani/util/types.testitem", []testitem{{int(7), "int", 0}, {int(8), "int", 2}}},
+	{testvec0{}, "github.com/fcavani/util/types.testvec0", testvec0{1, 2, 3, 4}},
+	{map[string]string{}, "map[string]string", map[string]string{"a": "1", "b": "2"}},
+	{map[string]string{}, "map[string]string", map[string]string{}},
+	//{&testvec0{}, "*serialization/types.testvec0", &testvec0{1,2,3,4}},
+	{teststr("oi"), "github.com/fcavani/util/types.teststr", teststr("oi")},
+	{make(channel), "github.com/fcavani/util/types.channel", make(channel)},
+	{new(channel), "*github.com/fcavani/util/types.channel", new(channel)},
+}
+
+func TestNameOf(t *testing.T) {
+	//print("\nTestNameOf\n\n")
+	for i, test := range tests {
+		s := NameOf(reflect.ValueOf(test.t).Type())
+		if s != test.name {
+			t.Fatalf("NameOf %v failed: %v != %v", i, test.name, s)
+		}
+	}
+}
+
+func TestIsert(t *testing.T) {
+	for _, typ := range tests {
+		Insert(typ.t)
+	}
+}
+
+func TestMake(t *testing.T) {
+	for i, typ := range tests {
+		val := MakeNew(typ.name, 0)
+		//println("can set:", val.CanSet())
+		name := NameOf(val.Type())
+		//println(name)
+		if name != typ.name {
+			t.Fatalf("type name differ in %v: %v != %v", i, name, typ.name)
+		}
+		//println(i)
+		val.Set(reflect.ValueOf(typ.val))
+		if !reflect.DeepEqual(val.Interface(), typ.val) {
+			t.Fatalf("not equal: %v", i)
+		}
+	}
+}
+
+func TestCopyBool(t *testing.T) {
+	b := true
+	cp := Copy(reflect.ValueOf(b))
+	b = false
+	if cp.Bool() != true {
+		t.Fatal("copy failed")
+	}
+}
+
+func TestCopyInt(t *testing.T) {
+	var i int = -42
+	cp := Copy(reflect.ValueOf(i))
+	i = 0
+	if cp.Interface().(int) != -42 {
+		t.Fatal("copy failed")
+	}
+	var i8 int8 = 1<<7 - 1
+	cp = Copy(reflect.ValueOf(i8))
+	i8 = 0
+	if cp.Interface().(int8) != 1<<7-1 {
+		t.Fatal("copy failed")
+	}
+	if cp.Kind() != reflect.Int8 {
+		t.Fatal("wrong kind")
+	}
+}
+
+func TestCopyUint(t *testing.T) {
+	var ui uint = 42
+	cp := Copy(reflect.ValueOf(ui))
+	ui = 0
+	if cp.Interface().(uint) != 42 {
+		t.Fatal("copy failed")
+	}
+}
+
+func TestCopyFloat32(t *testing.T) {
+	var f float32 = 3.1415
+	cp := Copy(reflect.ValueOf(f))
+	f = 0.0
+	if cp.Interface().(float32) != 3.1415 {
+		t.Fatal("copy failed")
+	}
+}
+
+func TestCopyComplex(t *testing.T) {
+	var c complex64 = complex(1, 1)
+	cp := Copy(reflect.ValueOf(c))
+	c = 0.0
+	if cp.Interface().(complex64) != complex(1, 1) {
+		t.Fatal("copy failed")
+	}
+}
+
+func TestCopyPtr(t *testing.T) {
+	var ui uint = 42
+	pui := &ui
+	cp := Copy(reflect.ValueOf(pui))
+	*pui = 0
+	if *(cp.Interface().(*uint)) != 42 {
+		t.Fatal("copy failed")
+	}
+}
+
+func TestCopyPtrNil(t *testing.T) {
+	var ui uint = 42
+	var pui *uint
+	cp := Copy(reflect.ValueOf(pui))
+	pui = &ui
+	if cp.Interface().(*uint) != nil {
+		t.Fatal("copy failed")
+	}
+}
+
+type IntForCopy interface {
+	Foo(x string) string
+}
+
+type CopyTextStruct struct {
+	Name  string
+	Val   float32
+	Ref   *CopyTextStruct
+	dummy complex64
+}
+
+func (c *CopyTextStruct) Foo(x string) string {
+	c.Name = x
+	return x
+}
+
+func TestCopyStruct1(t *testing.T) {
+	s := CopyTextStruct{
+		Name: "foo",
+		Val:  2.2,
+		Ref:  nil,
+	}
+	cp := Copy(reflect.ValueOf(s))
+	s.Name = "blá"
+	n := cp.Interface().(CopyTextStruct)
+	if n.Name != "foo" || n.Val != 2.2 || n.Ref != nil {
+		t.Fatalf("copy failed: %#v %#v", cp.Interface(), s)
+	}
+}
+
+func TestCopyStruct2(t *testing.T) {
+	ps := &CopyTextStruct{
+		Name: "foo",
+		Val:  2.2,
+		Ref:  nil,
+	}
+	ps.Ref = ps
+	cp := Copy(reflect.ValueOf(ps))
+	if !reflect.DeepEqual(cp.Interface(), ps) {
+		t.Fatalf("copy failed: %#v", cp.Interface())
+	}
+}
+
+func TestCopyArray(t *testing.T) {
+	a := [3]int{1, 2, 3}
+	cp := Copy(reflect.ValueOf(a))
+	a[0] = 0
+	acp := cp.Interface().([3]int)
+	if acp[0] != 1 || acp[1] != 2 || acp[2] != 3 {
+		t.Fatal("copy failed")
+	}
+}
+
+func TestCopyMap(t *testing.T) {
+	m := map[string]string{
+		"foo":   "bar",
+		"test1": "test2",
+		"blá":   "blá",
+	}
+	cp := Copy(reflect.ValueOf(m))
+	m["foo"] = "catoto"
+	mcp := cp.Interface().(map[string]string)
+	if mcp["foo"] != "bar" || mcp["test1"] != "test2" || mcp["blá"] != "blá" {
+		t.Fatal("copy failed")
+	}
+}
+
+func TestCopySlice(t *testing.T) {
+	s := []int{1, 2, 3}
+	cp := Copy(reflect.ValueOf(s))
+	s[0] = 0
+	scp := cp.Interface().([]int)
+	if scp[0] != 1 || scp[1] != 2 || scp[2] != 3 {
+		t.Fatal("copy failed")
+	}
+}
+
+func TestCopyInterface1(t *testing.T) {
+	ps := &CopyTextStruct{
+		Name: "foo",
+		Val:  2.2,
+		Ref:  nil,
+	}
+	ps.Ref = ps
+	var i IntForCopy = ps
+	cp := Copy(reflect.ValueOf(i))
+	if cp.Kind() != reflect.Ptr {
+		t.Fatal("wrong kind", cp.Kind())
+	}
+	if !reflect.DeepEqual(cp.Interface(), i) {
+		t.Fatalf("copy failed: %#v", cp.Interface())
+	}
+	i.Foo("new name")
+	cpps := cp.Interface().(*CopyTextStruct)
+	if cpps.Name != "foo" || cpps.Val != 2.2 {
+		t.Fatalf("copy failed: %#v", cp.Interface())
+	}
+}
+
+func TestCopyInterface2(t *testing.T) {
+	var i IntForCopy
+	cp := Copy(reflect.ValueOf(i))
+	if cp.IsValid() {
+		t.Fatal("very wrong")
+	}
+}
+
+type TestInterface struct {
+	Name string
+	Int  interface{}
+}
+
+func TestCopyInterface3(t *testing.T) {
+	s := &TestInterface{
+		Name: "foo",
+	}
+	cp := Copy(reflect.ValueOf(s))
+	if !reflect.DeepEqual(cp.Interface(), s) {
+		t.Fatalf("copy failed: %#v", cp.Interface())
+	}
+}
+
+func TestAnySettableValue(t *testing.T) {
+	ti := &TestInterface{
+		Name: "foo",
+	}
+	if !AnySettableValue(reflect.ValueOf(ti)) {
+		t.Fatal("AnySettableValue failed.")
+	}
+	err := errors.New("foo")
+	if AnySettableValue(reflect.ValueOf(err)) {
+		t.Fatal("AnySettableValue failed.")
+	}
+}
+
+type NotRecursive1 int
+
+type NotRecursive2 struct {
+	A string
+}
+
+type Recursive1 struct {
+	ptr *Recursive1
+}
+
+func TestIsRecursive(t *testing.T) {
+	if isRecursive(reflect.ValueOf(NotRecursive1(0))) {
+		t.Fatal("recursive")
+	}
+	if isRecursive(reflect.ValueOf(NotRecursive2{})) {
+		t.Fatal("recursive")
+	}
+	if isRecursive(reflect.ValueOf(&NotRecursive2{})) {
+		t.Fatal("recursive")
+	}
+	if !isRecursive(reflect.ValueOf(Recursive1{})) {
+		t.Fatal("not recursive")
+	}
+	if !isRecursive(reflect.ValueOf(&Recursive1{})) {
+		t.Fatal("not recursive")
+	}
+}
